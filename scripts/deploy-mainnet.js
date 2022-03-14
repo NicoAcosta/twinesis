7c6724e72c14198eef10faef018ced714c0584d4
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2022-03-14 04:50:18 -0300

    mainnet deploy

--- scripts/deploy-mainnet.js
+++ scripts/deploy-mainnet.js
@@ -10,12 +10,24 @@ async function main() {
 
 	console.log(unrevealedRaritiesBaseURI)
 
+	const revealedRaritiesBaseURI =
+		'https://ipfs.io/ipfs/' +
+		require('../metadata/revealed/uris').json +
+		'/'
+
+	console.log(revealedRaritiesBaseURI)
+
 	const Contract = await hre.ethers.getContractFactory('Twinesis')
 	const contract = await Contract.deploy(
 		unrevealedRaritiesBaseURI,
+		revealedRaritiesBaseURI,
 		process.env.MAINNET_WITHDRAWAL_1,
-		process.env.MAINNET_WITHDRAWAL_2
+		process.env.MAINNET_WITHDRAWAL_2,
+		{gasPrice: 19000000000}
 	)
+
+	const tx = contract.deployTransaction
+	console.log('Deployment tx:', tx.hash)
 
 	await contract.deployed()
 	;[deployer, ...addrs] = await ethers.getSigners()
